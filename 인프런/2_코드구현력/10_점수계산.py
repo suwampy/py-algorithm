aac70f1646f8dc358593cd82cf95d64ce44620df
suwampy <tndhks123@nate.com>
2021-05-09 22:35:57 +0900

    인프런-코드구현력-점수계산

---
+++ 인프런/2_코드구현력/10_점수계산.py
@@ -0,0 +1,18 @@
+import sys
+
+def 점수계산():
+    n = int(sys.stdin.readline())
+    a = list(map(int, input().split()))
+
+    sum=0
+    cnt=0
+    for i in a:
+        if x==1:
+            cnt+=1
+            sum+=cnt
+        else:
+            cnt=0
+
+
+if __name__ == "__main__":
+    점수계산()
