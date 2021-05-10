77c34cf780d97061023afded593289f11a391750
suwampy <tndhks123@nate.com>
2021-05-10 18:43:53 +0900

    인프런-이분탐색그리디-이분탐색

---
+++ 인프런/4_이분탐색그리디/1_이분검색.py
@@ -0,0 +1,23 @@
+import sys
+
+def 이분검색():
+    n,m = map(int,input().split())
+    a = list(map(int, input().split()))
+    a.sort()
+
+    lt = 0
+    rt = n-1
+    mid = (lt + rt) // 2
+
+    while lt<=rt:
+        mid = (lt + rt) // 2
+        if a[mid]==m:
+            print(mid+1)
+            break;
+        elif a[mid]>m:
+            rt = mid-1
+        else:
+            lt = mid+1
+
+if __name__ =="__main__":
+    이분검색()
