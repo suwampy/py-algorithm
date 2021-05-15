78335922b84932fcae1b534c3fead606cf3f43bb
suwampy <tndhks123@nate.com>
2021-05-15 22:22:54 +0900

    백준-이분탐색-2417_정수제곱근

---
+++ 백준/이분탐색/2417_정수제곱근.py
@@ -0,0 +1,20 @@
+import sys
+
+def 정수제곱근():
+    n = int(input())
+
+    lt = 1
+    rt = n
+    res = 0
+    while lt<=rt :
+        mid = (lt+rt)//2
+        if mid*mid <= n:
+            lt = mid +1
+        else:
+            rt = mid - 1
+            res = mid
+    print(res)
+
+
+if __name__ =="__main__":
+    정수제곱근()
