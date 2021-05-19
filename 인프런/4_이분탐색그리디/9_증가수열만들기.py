91e652484cf616ca75678cc85330ff1a6c79ba82
suwampy <tndhks123@nate.com>
2021-05-20 00:42:26 +0900

    인프런-이분탐색그리디-증가수열

--- 인프런/4_이분탐색그리디/9_증가수열만들기.py
+++ 인프런/4_이분탐색그리디/9_증가수열만들기.py
@@ -4,13 +4,31 @@ from collections import deque
 def 증가수열():
     n = int(input())
     a = list(map(int, input().split()))
-    a=deque(a)
+    lt=0
+    rt=n-1
+    last =0
+    res="" #문자열
 
-    s = ""
+    tmp=[] #빈 list
 
-
-    print(s)
-
+    while lt<=rt:
+        if a[lt]>last:
+            tmp.append((a[lt],'L'))
+        if a[rt]<last:
+            tmp.append((a[rt],'R'))
+        tmp.sort()
+        if len(tmp)==0:
+            break
+        else:
+            res=res+tmp[0][1]
+            last=tmp[0][0]
+            if tmp[0][1]=='L':
+                lt+=1
+            else:
+                rt+=1
+        tmp.clear()
+    print(len(res()))
+    print(res)
 
 if __name__ =="__main__":
     증가수열()
